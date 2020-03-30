a663104b0f6037ea15c4b33512a3652a69edaf3b
athena314320 <55180760+athena314320@users.noreply.github.com>
2020-03-31 03:38:07 +0800

    331半完成黨

---
+++ ML_Footballl/Assets/Scrips/Ball.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    /// <summery>
+    /// 足球是否進入球門
+    /// </summery>
+
+    public static bool complete;
+
+    /// <summery>
+    /// 觸發開始事件 :碰到勾選 Is Trigger 物件會執行一次
+    /// </summery>
+    /// <parm name ="other">碰到的物件碰撞資訊</parm>
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //如果 碰到物件  的  名稱  等於 "進球感應區"
+
+        if (other.name == "進球感應區")
+        {
+            //進入球門
+            complete = true;
+        }
+    }
+}
